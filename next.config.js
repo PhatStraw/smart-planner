1c6382efad1f212ed3775d228ad246744a331c06
PhatStraws <kevindsims1@gmail.com>
2023-10-08 22:11:45 -0700

    far

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    OPENAI_API: process.env.OPENAI_API
+  }
 }
 
 module.exports = nextConfig
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import 'dotenv/config'
+import Head from 'next/head'
+import OpenAI from 'openai';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+export default async function handler (req, res)  {
+  const response = await openai.predict(`Plan my trip to ${req.body.destination}. My budget is ${req.body.budget}, and I prefer a ${req.body.activity} level experience with a focus on ${req.body.interest}. I will be traveling from ${req.body.startDate} to ${req.body.endDate}. Also take this into consideration: ${req.body.sideNote}`);
+  const data = response.data.response
+  res.status(200).send(data)
 }
