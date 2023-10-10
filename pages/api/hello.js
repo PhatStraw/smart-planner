027b4e3f123b4fe0a00db22fe0088f152f0585fc
PhatStraws <kevindsims1@gmail.com>
2023-10-10 11:07:11 -0700

    updated-prompt

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,8 +7,25 @@ const openai = new OpenAI({
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const response = await openai.predict(`Act as a professional travel planner. Ill give you details on my trip and respond with detailed plans in the form of an itinerary based around the dates and information I give you. Include names of stores, places and anything else you plan. Be specefic. make sure to include atleast 2 sentences worth of activities per day. Double check your responses to make sure its a valid json object. Your goal is to create the bext imaginable expierience with the data I give you. Provide your response in JSON format only. The format should be a JSON object like {"itinerary": [{"day": 1, "activity": "Take a Visit to the Golden Gate bridge and expience the wonder it has to offer."}]}. Provide no other commentary. Make sure there are no newline characters or spaces in the JSON object response. Plan my trip to ${req.body.destination}. My budget is ${req.body.budget}, and I prefer a ${req.body.activity} level experience with a focus on ${req.body.interest}. I will be traveling from ${req.body.startDate} to ${req.body.endDate}. Also take this into consideration: ${req.body.sideNote}.`);
+    const response = await openai.predict(
+    `Act as a specialized travel planner. Utilize the provided trip details to craft a meticulously designed itinerary. 
+     Your response should feature daily activities, ensuring a rich experience. 
+     Include specific names of venues, establishments, and attractions. 
+     Each day should comprise at least two distinct activities. 
+     Ascertain the validity of your response as a JSON object with no extraneous whitespace or newline characters. 
+     Adhere to the following format: {"itinerary": [{"day": 1, "activity": "planned activity"}]}. 
+     Respond solely in JSON format, refraining from additional commentary.
+    
+     Here are the trip details: 
+     Plan a journey to ${req.body.destination}, 
+     within a budget of ${req.body.budget}. 
+     Tailor the experience to a ${req.body.activity} level of activity, 
+     emphasizing ${req.body.interest}. 
+     The travel period spans from ${req.body.startDate} to ${req.body.endDate}. 
+     Incorporate the following note: ${req.body.sideNote}.
+    `);
     const data = response
+    console.log(data)
     res.status(200).json({ data })
   } else {
     res.status(405).json({ error: 'Method not allowed' });
