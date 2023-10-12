51d21f5e8e75d164a18e88abfa7bf54180458b33
PhatStraw <kevindsimsjr@gmail.com>
2023-10-12 11:04:18 -0700

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import 'components/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Roboto } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Roboto({ subsets: ['latin'],weight: '500' })
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
