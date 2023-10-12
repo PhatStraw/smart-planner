1a5e47b23667a682fd7fbbb864d9dbbba831c262
PhatStraw <kevindsimsjr@gmail.com>
2023-10-12 13:16:41 -0700

    restyle

--- pages/index.js
+++ pages/index.js
@@ -56,8 +56,7 @@ function Home() {
   return (
 
     <div className="flex flex-no-wrap">
-      {/* <!-- Sidebar starts -->
-      <!-- Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] --> */}
+      {/* <!-- Sidebar starts -->*/}
       <SideBar
         makePlans={makePlans}
         setActivity={setActivity}
@@ -71,12 +70,9 @@ function Home() {
         interest={interest}
         setSideNote={setSideNote}
       />
-      {/* <!-- Sidebar ends -->
-      <!-- Remove class [ h-64 ] when adding a card block --> */}
+      {/* <!-- Sidebar ends -->*/}
       <div className="container mx-auto py-10 h-[100vh] md:w-4/5 w-11/12">
-        {/* <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border --> */}
-        <div className="w-full h-[100%] rounded border-dashed border-2 border-gray-300">
-          {/* <!-- Place your content here --> */}
+        <div className="w-full h-[100%] rounded">
           <div className="container mx-auto py-10 h-full md:w-4/5 w-11/12 px-6 overflow-hidden overflow-y-auto">
             {!loading ? (
               <div className="w-full flex flex-col flex-grow  overflow-auto mb-2 text-white">
