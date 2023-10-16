4dc0ae9a5758df87147fb6c45cd61e72f5542691
PhatStraw <kevindsimsjr@gmail.com>
2023-10-16 14:10:54 -0700

    styles

--- pages/api/plan.js
+++ pages/api/plan.js
@@ -1,8 +1,18 @@
 import { openAiExtract } from '../../utils/OpenAIExtract.js';
-import { getPhotos } from '../../utils/UnsplashRandom.js';
 export const config = {
   runtime: "edge",
 };
+
+export async function getPhotos(query) {
+  const response = await fetch(`https://api.unsplash.com/photos/random?query=${query}&client_id=${process.env.NEXT_UNSPLASH_ACCESS}`);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok ' + response.statusText);
+  }
+  
+  const data = await response.json();
+  return data.urls.regular;
+}
 
 export default async function handler(req) {
   const body = await req.json()
