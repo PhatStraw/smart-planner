41c5e2c9b37fa5fdd031a02285389c1551b7d534
PhatStraw <kevindsimsjr@gmail.com>
2023-10-20 09:06:41 -0700

    errorfix

--- pages/selected.js
+++ pages/selected.js
@@ -83,7 +83,7 @@ console.log(JSON.stringify(plansState.selectedPlan))
     return (
         <div>
             <NavBar />
-            {plansState.selectedPlan ? (
+            {plansState.selectedPlan.length > 0 ? (
                 <div className="m-auto pt-24 rounded w-[100%] max-w-[1280px]">
                     <Carousel showThumbs={false} showStatus={false} showArrows={false}>
                         {plansState.selectedPlan.image.map((i) => (
